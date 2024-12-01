fa8d0ed37dfa7d2883a17b2c5c6e6b66fb1e8989
wallace rutledge <shonuffoo7@yahoo.com>
2024-12-01 13:54:51 -0600

    99. Add toastr to Project

---
+++ Data/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace YumBlazor.Data.Repository.IRepository;
+
+public interface ICategoryRepository
+{
+    public Category Create(Category obj);
+    public Category Update(Category obj);
+    public bool Delete(int id);
+    public Category Get(int id);
+    public IEnumerable<Category> GetAll();
+
+}
